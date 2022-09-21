7f74c256882ac48287e98795e7b649e3d40a62c9
andrea-prina <and.prina@gmail.com>
2022-09-21 16:33:56 +0200

    Create relationship between User and Post model

--- app/models/Post.php
+++ app/models/Post.php
@@ -9,8 +9,13 @@ class Post extends Model
     protected $fillable = [
         'author',
         'title',
+        'user_id',
         'post_content',
         'post_image',
         'post_date',
     ];
+
+    public function user(){
+        return $this->belongsTo('app\User');
+    }
 }
